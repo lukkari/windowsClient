03b1c5db8668181c4f5ac0c40719ba45a0077adf
denis_anchugov <denis.anchugov@me.com>
2015-01-02 00:52:40 +0200

    Implement timeline structure for displaying the timetable. Refactoring.

---
+++ Schedule.UniversalApp/Schedule.UniversalApp.WindowsPhone/SchedulePresenter.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+using Schedule.UniversalApp.Model.ScheduleEntities;
+
+namespace Schedule.UniversalApp
+{
+    public sealed partial class SchedulePresenter : UserControl
+    {
+        public SchedulePresenter()
+        {
+            this.InitializeComponent();
+            this.DataContextChanged += SchedulePresenter_DataContextChanged;
+        }
+
+        private void SchedulePresenter_DataContextChanged(FrameworkElement sender, DataContextChangedEventArgs args)
+        {
+            var timetable = args.NewValue as DaySchedule;
+            RenderTimetable(timetable);
+        }
+
+        private void RenderTimetable(DaySchedule timetable)
+        {
+            if (timetable == null) return;
+            for (var i = 7; i < 20; )
+            {
+                foreach (var subject in timetable.Subjects)
+                {
+                    if (subject.Date.Hour != i) continue;
+                    DayPanel.Children.Add(new SubjectPanel(subject));
+                    i = i + subject.Duration;
+                }
+                DayPanel.Children.Add(new SubjectPanel(i));
+                i++;
+            }
+        }
+    }
+}
---
+++ Schedule.UniversalApp/Schedule.UniversalApp.WindowsPhone/SubjectPanel.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+
+using Schedule.UniversalApp.Model.ScheduleEntities;
+
+namespace Schedule.UniversalApp
+{
+    public sealed partial class SubjectPanel : UserControl
+    {
+        public SubjectPanel(ScheduleEntity scheduleEntity)
+        {
+            this.InitializeComponent();
+            StartTime.Text = scheduleEntity.GetStringDate;
+            Duration.Text = scheduleEntity.Duration + "h.";
+            EndTime.Text = scheduleEntity.GetStringEndTime;
+            SetSize(scheduleEntity.Duration);
+            DescriptionPanel.Text = scheduleEntity.Subject.Name;
+            Room.Text = string.Join(",", scheduleEntity.Rooms);
+            TeacherName.Text = string.Join(",", scheduleEntity.Teachers);
+        }
+
+        //TODO: Needs better implementation.
+        public SubjectPanel(int startingTime)
+        {
+            this.InitializeComponent();
+            StartTime.Text = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, startingTime, 15, 0).ToString("HH:mm");
+            EndTime.Text = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, startingTime + 1, 0, 0).ToString("HH:mm");
+            SetSize();
+        }
+      
+        private void SetSize(int duration = 1)
+        {
+            RootGrid.Height = 60 * duration;
+        }
+    }
+}
